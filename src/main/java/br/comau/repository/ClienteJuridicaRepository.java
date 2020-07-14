f9fdc7c008798d20a814cca3733e859260511269
Herick <herick.marques@gmail.com>
2020-07-13 21:32:06 -0300

    Correção no chamada da classe

--- src/main/java/br/comau/repository/ClienteJuridicaRepository.java
+++ src/main/java/br/comau/repository/ClienteJuridicaRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ClienteJuridicaRepository extends MongoRepository<ClienteJuridica, Long> {
 
-   ClienteFisica findByNomeFantasia(String nomeFantasia);
+   ClienteJuridica findByNomeFantasia(String nomeFantasia);
 
 }
